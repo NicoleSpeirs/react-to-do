a81fb6226e4dc192d78a3fe943aa58ebd22257e8
Nicole Speirs <nickispeirs@gmail.com>
2019-01-29 13:59:29 -0600

    CHECKPOINT: React To-Do add onChange event listener

--- src/App.js
+++ src/App.js
@@ -16,12 +16,15 @@ class App extends Component {
     };
   }
 
+  toggleComplete() {
+    console.log('toggleComplete executed');
+}
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map((todo, index) =>
-            <ToDo key={ index }  description={ todo.description } isCompleted={ todo.isCompleted }/>
+            <ToDo key={ index }  description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
           )}
         </ul>
       </div>
